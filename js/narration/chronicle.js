29a7e9f13fd16ae40103b8ecbe97847001336ec9
rng-803 <rodngarcia@gmail.com>
2026-05-26 05:50:59 -0300

    Refactor project structure into html css config api narration presets gameplay

---
+++ js/narration/chronicle.js
@@ -0,0 +1,3 @@
+// Narration module placeholder.
+// Chronicle/log panel functions are currently defined in js/gameplay/core.js.
+// Future refactors should migrate narration state and log rendering here.
